d4a037f8a13ad1f875edbe6fcd1fc1b5eab17d04
Sib <chengjie.michael.huang@gmail.com>
2025-12-24 18:29:58 -0800

    removed description

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Merry Christmas you filthy animal",
-  description: "A 3D rotating coal with holy Christmas light reveal",
 };
 
 export default function RootLayout({
